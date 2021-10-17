fca5e7a05f5a2b10c42c2c5f99d4a363c6c8b688
ejay11 <ellen.jaquette@gmail.com>
2021-10-16 21:18:42 -0500

    trying method with loop

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,11 +6,24 @@ def bubble_sort(array)
 
 
 #Other method without max enumerable - not sure if above counts as bubble sorting
-sorted_array = []
-i = 0
-while i < array.length do
-  print array.each_with_index {|number, index| number <=> array[index+1]}
-  #array.each |number|
+x = 0
+
+while x < array.length do
+  array.each_with_index do |number, index|
+    check_index = (index + 1) - array.length
+    puts "The index to compare is #{index} and then #{check_index}"
+    if check_index == 0 
+      return array
+    elsif number > array[check_index]
+      puts "#{number} is greater than #{array[check_index]}"
+      move = array.delete_at(index) 
+      array.insert(check_index, number)
+      print array
+    end
 end
+x += 1
 end
+print array
+end
+
 bubble_sort([4,3,78,2,0,2])
