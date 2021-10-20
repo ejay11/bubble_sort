582a03ee0aa9021dcf00b8c0e398cf75e4376ccb
ejay11 <ellen.jaquette@gmail.com>
2021-10-19 21:45:14 -0500

    second sort method added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,23 +7,25 @@ def bubble_sort(array)
 
 #Other method without max enumerable - not sure if above counts as bubble sorting
 x = 0
-
-while x < array.length do
+def check(array)
   array.each_with_index do |number, index|
     check_index = (index + 1) - array.length
-    puts "The index to compare is #{index} and then #{check_index}"
     if check_index == 0 
       return array
     elsif number > array[check_index]
-      puts "#{number} is greater than #{array[check_index]}"
       move = array.delete_at(index) 
       array.insert(check_index, number)
-      print array
     end
-end
+  end
+  end
+
+while x < array.length do
+  check(array)
 x += 1
 end
+
 print array
+
 end
 
 bubble_sort([4,3,78,2,0,2])
